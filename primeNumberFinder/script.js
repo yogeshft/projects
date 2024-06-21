663fdda0f2d4f2bcb25f45b9744e1f38541f1e7e
yogesh <yadavyogesh404@gmail.com>
2024-06-22 01:15:32 +0530

    handled all cases

--- primeNumberFinder/script.js
+++ primeNumberFinder/script.js
@@ -1,13 +1,12 @@
 "use strict";
 
-// dom
+// DOM elements
 let primeNumberInput = document.querySelector("#primeNumberInput");
 const clearBtn = document.querySelector("#clearResult");
 const formSubmit = document.querySelector("#primeNumberForm");
-const submitBtn = document.getElementById("submit");
 let output = document.getElementById("outputValue");
 
-// logic
+// Event listeners
 document.addEventListener("DOMContentLoaded", () => {
   formSubmit.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -15,33 +14,46 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+primeNumberInput.addEventListener("input", (e) => {
+  let pattern = /^\d*$/;
+  if (!pattern.test(e.target.value)) {
+    e.target.value = e.target.value.replace(/\D/g, "");
+  }
+});
+
 clearBtn.addEventListener("click", () => handleClearInput(primeNumberInput));
 
+// Functions
 function handleClearInput(input) {
   input.value = "";
+  output.textContent = ""; // Clear output when clearing input
 }
 
 function handleFormSubmission() {
   let primeValue = parseInt(primeNumberInput.value.trim());
-  let isValid = isFormValid(primeValue);
-  if (isValid) {
-    displayResult(primeValue);
+  if (primeValue) {
+    let isValid = isFormValid(primeValue);
+    if (isValid && checkPrimeNumber(primeValue)) {
+      displayResult(primeValue, true);
+    } else {
+      displayResult(primeValue, false);
+      console.log("form invalid");
+    }
   } else {
-    console.log("form invalid");
+    return;
   }
 }
 
 function isFormValid(inputValue) {
-  return typeof inputValue === "number" ? true : false;
+  return !isNaN(inputValue) && inputValue > 1; // Valid if it's a number greater than 1
 }
 
-function displayResult(number) {
-  let primeNumber = checkPrimeNumber(number);
-  output.textContent = primeNumber ? "Prime Number!" : "Not a Prime Number!";
+function displayResult(number, isPrime) {
+  output.textContent = isPrime ? "Prime Number!" : "Not a Prime Number!";
 }
 
 function checkPrimeNumber(number) {
-  if (number <= 1) {
+  if (isNaN(number) || number <= 1 || number == "") {
     return false;
   }
   if (number <= 3) {
@@ -50,11 +62,12 @@ function checkPrimeNumber(number) {
   if (number % 2 === 0 || number % 3 === 0) {
     return false;
   }
-  let sqrtNum = Math.sqrt(number);
-  for (let i = 5; i <= sqrtNum; i += 6) {
+  let i = 5;
+  while (i * i <= number) {
     if (number % i === 0 || number % (i + 2) === 0) {
       return false;
     }
+    i += 6;
   }
   return true;
 }
